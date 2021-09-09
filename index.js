dfca83c0763fafb95d07300ef390f3321f4997c4
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-09-09 12:13:24 +0300

    removal of react

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app = express();
-var cors = require("cors");
 
 const port = process.env.PORT || 8080;
 
 // näytä palvelimella sellaisenaan kaikki
 // tiedostot jotka löytyy hakemistosta public
 app.use(express.static("public"));
-app.use(express.static("public/static"));
-
-app.use(cors());
 
 let db = [
   { id: 1, name: "jack" },
