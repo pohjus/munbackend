d48d79da8bd39fa1b74a0ed509c89b57b8cd95b3
Jussi Pohjolainen <jussi.pohjolainen@gmail.com>
2021-09-09 12:02:16 +0300

    cors enabled

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
+
 const port = process.env.PORT || 8080;
 
 // näytä palvelimella sellaisenaan kaikki
 // tiedostot jotka löytyy hakemistosta public
 app.use(express.static("public"));
+app.use(cors());
 
 let db = [
   { id: 1, name: "jack" },
